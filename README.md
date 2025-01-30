0029d1196663748194f94ffcda47b594502cf661
sarina-tan <sarina.tan@emory.edu>
2025-01-29 23:23:35 -0500

    Homework1-2

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Three steps (oversimplified)
 - Add (`git add .`)
 - Commit (`git commit -m ...`)
 - Push (`git push`)
+
+\begin{tabular}{lr}
+{
+    
+}
---
+++ homework1/submission2/data-code/_BuildFinalData.py
@@ -0,0 +1,12 @@
+# Meta --------------------------------------------------------------------
+# Author:        Sarina Tan
+# Date Created:  1/29/2025
+# Date Edited:   1/29/2025
+# Notes:         Python file to build Medicare Advantage dataset for Econ/HLTH 470 Hwk 1
+
+
+# Call individual scripts -------------------------------------------------
+
+exec(open("/Users/sarinatan/Desktop/HLTH470/homework1/submission2/data-code/1_Plan_Data.py").read())
+
+exec(open("/Users/sarinatan/Desktop/HLTH470/homework1/submission2/data-code/2_Service_Areas.py").read())
